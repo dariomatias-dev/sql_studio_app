d746683669594192a49778caedc7ffcc57a472f2
dariomatias-dev <matiasdario75@gmail.com>
2025-10-26 16:14:33 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# sql_studio
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/br/com/dariomatias/sql_studio/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.dariomatias.sql_studio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
