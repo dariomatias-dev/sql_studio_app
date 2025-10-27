76f7add755424749fcc14e7ce461e6f5901337e9
dariomatias-dev <matiasdario75@gmail.com>
2025-10-27 14:33:06 -0300

    feat(lib/src/shared/widgets): created confirmation dialog component
    - Developed a confirmation dialog component to simplify action confirmation scenarios.

---
+++ lib/src/shared/widgets/dialogs/confirmation_dialog_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:sql_studio/src/shared/widgets/cancel_button_widget.dart';
+import 'package:sql_studio/src/shared/widgets/dialog_widget.dart';
+
+class ConfirmationDialogWidget extends StatelessWidget {
+  const ConfirmationDialogWidget({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.confirmButton,
+  });
+
+  final String title;
+  final String description;
+  final Widget confirmButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return DialogWidget(
+      title: title,
+      content: Text(
+        description,
+        textAlign: TextAlign.center,
+      ),
+      actions: <Widget>[
+        CancelButtonWidget(),
+        confirmButton,
+      ],
+    );
+  }
+}
