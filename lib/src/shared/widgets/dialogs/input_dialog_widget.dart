8dc7a117576d1a0da8c8ac211cbe05bae4337d80
dariomatias-dev <matiasdario75@gmail.com>
2025-10-27 15:10:22 -0300

    feat(lib/src/shared/widgets/dialogs/input_dialog_widget.dart): created text input dialog
    - Developed a text input dialog to simplify and standardize dialogs for text input.

---
+++ lib/src/shared/widgets/dialogs/input_dialog_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'package:sql_studio/src/shared/widgets/button_widget.dart';
+import 'package:sql_studio/src/shared/widgets/cancel_button_widget.dart';
+import 'package:sql_studio/src/shared/widgets/dialogs/dialog_widget.dart';
+import 'package:sql_studio/src/shared/widgets/input_widget.dart';
+
+class InputDialogWidget extends StatefulWidget {
+  const InputDialogWidget({
+    super.key,
+    required this.title,
+    required this.controller,
+    required this.label,
+    required this.onSubmit,
+    this.validator,
+    this.submitText = 'Submit',
+  });
+
+  final String title;
+  final TextEditingController controller;
+  final String label;
+  final String? Function(String?)? validator;
+  final void Function(String value) onSubmit;
+  final String submitText;
+
+  @override
+  State<InputDialogWidget> createState() => _InputDialogWidgetState();
+}
+
+class _InputDialogWidgetState extends State<InputDialogWidget> {
+  final formKey = GlobalKey<FormState>();
+
+  void _handleSubmit() {
+    if (!formKey.currentState!.validate()) return;
+
+    widget.onSubmit(widget.controller.text);
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DialogWidget(
+      title: widget.title,
+      content: Form(
+        key: formKey,
+        child: InputWidget(
+          controller: widget.controller,
+          labelText: widget.label,
+          onChanged: (value) => setState(() {}),
+          validator:
+              widget.validator ??
+              (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return 'This field is required';
+                }
+
+                return null;
+              },
+        ),
+      ),
+      actions: <Widget>[
+        const CancelButtonWidget(),
+        ButtonWidget(
+          onPressed: _handleSubmit,
+          text: widget.submitText,
+          style: ButtonStyleType.black,
+        ),
+      ],
+    );
+  }
+}
