9115094c8088792754b3c26bb676c509e06dadfb
dariomatias-dev <matiasdario75@gmail.com>
2025-10-26 21:01:17 -0300

    feat(lib/src/shared/widgets): created reusable text field component
    - Developed a reusable text field component for consistent and simplified usage.

---
+++ lib/src/shared/widgets/input_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class InputWidget extends StatelessWidget {
+  const InputWidget({
+    super.key,
+    this.onChanged,
+    this.controller,
+    this.hintText = '',
+    this.labelText,
+  });
+
+  final ValueChanged<String>? onChanged;
+  final TextEditingController? controller;
+  final String hintText;
+  final String? labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: labelText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          borderSide: BorderSide(color: Colors.grey.shade300, width: 0.1),
+        ),
+        isDense: true,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 12.0,
+          vertical: 12.0,
+        ),
+      ),
+      onChanged: onChanged,
+      onTapOutside: (event) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+    );
+  }
+}
