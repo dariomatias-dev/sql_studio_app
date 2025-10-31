fa87cac6fbb51726681b9f09b481719c03a9bb64
dariomatias-dev <matiasdario75@gmail.com>
2025-10-31 13:02:13 -0300

    feat(lib/src/screens/home/home_screen.dart): simplified screen
    - Implemented `SqlWorkspaceWidget` to simplify and standardize the screen.

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,84 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:sql_studio/src/shared/widgets/sql_workspace/console/console_widget.dart';
-import 'package:sql_studio/src/shared/widgets/sql_workspace/divider_bar_widget.dart';
-import 'package:sql_studio/src/shared/widgets/sql_workspace/sql_editor/sql_editor_widget.dart';
+import 'package:sql_studio/src/shared/widgets/sql_workspace/sql_workspace_widget.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  double _editorHeightFraction = 0.66;
-  bool _editorMaximized = false;
-  bool _consoleMaximized = false;
-
-  double get screenHeight => MediaQuery.of(context).size.height;
-
-  void _toggleEditorMaximize() {
-    setState(() {
-      _editorMaximized = !_editorMaximized;
-      _consoleMaximized = false;
-    });
-  }
-
-  void _toggleConsoleMaximize() {
-    setState(() {
-      _consoleMaximized = !_consoleMaximized;
-      _editorMaximized = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (_editorMaximized) {
-      return SqlEditorWidget(
-        isFullScreen: _editorMaximized,
-        onFullScreen: _toggleEditorMaximize,
-      );
-    }
-
-    if (_consoleMaximized) {
-      return ConsoleWidget(
-        isFullScreen: _consoleMaximized,
-        onFullScreen: _toggleConsoleMaximize,
-      );
-    }
-
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: (_editorHeightFraction * 100).toInt(),
-            child: SqlEditorWidget(
-              isFullScreen: _editorMaximized,
-              onFullScreen: _toggleEditorMaximize,
-            ),
-          ),
-          DividerBarWidget(
-            onDragUpdate: (details) {
-              setState(() {
-                final delta = details.delta.dy / screenHeight;
-                _editorHeightFraction = (_editorHeightFraction + delta).clamp(
-                  2 / 5,
-                  0.9,
-                );
-              });
-            },
-          ),
-          Flexible(
-            flex: ((1 - _editorHeightFraction) * 100).toInt(),
-            child: ConsoleWidget(
-              isFullScreen: _consoleMaximized,
-              onFullScreen: _toggleConsoleMaximize,
-            ),
-          ),
-        ],
-      ),
+      body: SqlWorkspaceWidget(),
     );
   }
 }
