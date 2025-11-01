14199ecf87c91ad6af77f2820e8ffb88cf1d8b1e
dariomatias-dev <matiasdario75@gmail.com>
2025-11-01 18:01:37 -0300

    feat(lib/src/screens/main/widgets/create_database_dialog_widget.dart): refactored component
    - Added fields for database label and name.
    - Enabled automatic filling of the name field based on the label.
    - Added clear icon to the input fields.

--- lib/src/screens/main/widgets/create_database_dialog_widget.dart
+++ lib/src/screens/main/widgets/create_database_dialog_widget.dart
@@ -4,8 +4,11 @@ import 'package:provider/provider.dart';
 import 'package:sql_studio/src/notifiers/database_notifier.dart';
 
 import 'package:sql_studio/src/shared/models/database_model.dart';
+import 'package:sql_studio/src/shared/widgets/cancel_button_widget.dart';
+import 'package:sql_studio/src/shared/widgets/dialogs/dialog_widget.dart';
+import 'package:sql_studio/src/shared/widgets/input_widget.dart';
+import 'package:sql_studio/src/shared/widgets/buttons/button_widget.dart';
 import 'package:sql_studio/src/shared/widgets/dialogs/error_dialog_widget.dart';
-import 'package:sql_studio/src/shared/widgets/dialogs/input_dialog_widget.dart';
 
 class CreateDatabaseDialogWidget extends StatefulWidget {
   const CreateDatabaseDialogWidget({super.key, required this.onCreated});
@@ -19,7 +22,12 @@ class CreateDatabaseDialogWidget extends StatefulWidget {
 
 class _CreateDatabaseDialogWidgetState
     extends State<CreateDatabaseDialogWidget> {
-  final controller = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  final labelController = TextEditingController();
+  final nameController = TextEditingController();
+
+  bool _nameEdited = false;
 
   String _toSnakeCase(String input) {
     return input
@@ -30,12 +38,13 @@ class _CreateDatabaseDialogWidgetState
         .toLowerCase();
   }
 
-  Future<bool> _createDatabase(String value) async {
-    final snakeName = _toSnakeCase(value);
+  Future<void> _handleCreate() async {
+    if (!formKey.currentState!.validate()) return;
 
-    final database = await context.read<DatabaseNotifier>().getByName(
-      snakeName,
-    );
+    final label = labelController.text.trim();
+    final name = nameController.text.trim();
+
+    final database = await context.read<DatabaseNotifier>().getByName(name);
 
     if (database != null) {
       if (mounted) {
@@ -43,50 +52,105 @@ class _CreateDatabaseDialogWidgetState
           context: context,
           builder: (context) {
             return ErrorDialogWidget(
-              description: 'Database "$snakeName" already exists',
+              description: 'Database "$name" already exists',
             );
           },
         );
       }
 
-      return false;
+      return;
     }
 
-    final createDatabase = DatabaseModel(label: value, name: snakeName);
+    final createDatabase = DatabaseModel(label: label, name: name);
 
     widget.onCreated(createDatabase);
 
-    if (mounted) {
-      Navigator.pop(context);
-    }
-
-    return true;
+    if (mounted) Navigator.pop(context);
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    labelController.dispose();
+    nameController.dispose();
 
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return InputDialogWidget(
+    return DialogWidget(
       title: 'Create Database',
-      controller: controller,
-      label: 'Database Name',
-      submitText: 'Create',
-      validator: (value) {
-        if (value == null || value.trim().isEmpty) {
-          return 'Please enter a database name';
-        } else if (!RegExp(r'^[a-zA-Z0-9_-]+$').hasMatch(_toSnakeCase(value))) {
-          return 'Invalid characters detected';
-        }
+      content: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            InputWidget(
+              controller: labelController,
+              labelText: 'Label',
+              suffixIcon: IconButton(
+                onPressed: () {
+                  labelController.clear();
+                  if (!_nameEdited) {
+                    nameController.clear();
+                  }
 
-        return null;
-      },
-      onSubmit: _createDatabase,
+                  setState(() {});
+                },
+                icon: const Icon(Icons.clear),
+              ),
+              onChanged: (value) {
+                if (!_nameEdited) {
+                  nameController.text = _toSnakeCase(value);
+                }
+
+                setState(() {});
+              },
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return 'Please enter a label';
+                }
+
+                return null;
+              },
+            ),
+            const SizedBox(height: 16),
+            InputWidget(
+              controller: nameController,
+              labelText: 'Name',
+              suffixIcon: IconButton(
+                onPressed: () {
+                  nameController.clear();
+                  _nameEdited = false;
+
+                  setState(() {});
+                },
+                icon: const Icon(Icons.clear),
+              ),
+              onChanged: (_) => _nameEdited = true,
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return 'Please enter a name';
+                } else if (!RegExp(
+                  r'^[a-z0-9_]+$',
+                ).hasMatch(_toSnakeCase(value))) {
+                  return 'Invalid characters detected';
+                }
+
+                return null;
+              },
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        CancelButtonWidget(),
+        ButtonWidget(
+          text: 'Create',
+          onPressed: _handleCreate,
+          style: ButtonStyleType.black,
+        ),
+      ],
     );
   }
 }
