ee71cdcc21a9a7c212b4f2bb6393c8c5a6877662
dariomatias-dev <matiasdario75@gmail.com>
2025-11-05 11:54:54 -0300

    feat(lib/src/core/types/sql_advanced_suggestion_model.dart): added new features
    - Added `id` property.
    - Created `copyWith`, `fromMap`, and `toMap` methods.

--- lib/src/core/types/sql_advanced_suggestion_model.dart
+++ lib/src/core/types/sql_advanced_suggestion_model.dart
@@ -1,11 +1,42 @@
+import 'package:uuid/uuid.dart';
+
 class SqlAdvancedSuggestionModel {
+  final String id;
   final String label;
   final String code;
   final String? selectText;
 
-  const SqlAdvancedSuggestionModel({
+  SqlAdvancedSuggestionModel({
+    String? id,
     required this.label,
     required this.code,
     this.selectText,
-  });
+  }) : id = id ?? const Uuid().v4();
+
+  SqlAdvancedSuggestionModel copyWith({
+    String? id,
+    String? label,
+    String? code,
+    String? selectText,
+  }) {
+    return SqlAdvancedSuggestionModel(
+      id: id ?? this.id,
+      label: label ?? this.label,
+      code: code ?? this.code,
+      selectText: selectText ?? this.selectText,
+    );
+  }
+
+  static SqlAdvancedSuggestionModel fromMap(Map<String, dynamic> map) {
+    return SqlAdvancedSuggestionModel(
+      id: map['id'] as String,
+      label: map['label'] as String,
+      code: map['code'] as String,
+      selectText: map['select_text'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'label': label, 'code': code, 'select_text': selectText};
+  }
 }
