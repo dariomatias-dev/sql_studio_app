8d10a50313170e60f727524bd49a2e50a1f1069f
dariomatias-dev <matiasdario75@gmail.com>
2025-11-07 09:00:45 -0300

    feat(lib/src/shared/widgets/sql_workspace/sql_editor/sql_suggestions_bars/sql_advanced_suggestions_bar_widget.dart): added usage of `SqlAdvancedSuggestionsNotifier`
    - Implemented the use of `SqlAdvancedSuggestionsNotifier` for listing advanced SQL suggestions.

--- lib/src/shared/widgets/sql_workspace/sql_editor/sql_suggestions_bars/sql_advanced_suggestions_bar_widget.dart
+++ lib/src/shared/widgets/sql_workspace/sql_editor/sql_suggestions_bars/sql_advanced_suggestions_bar_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'package:sql_studio/src/core/constants/sql_advanced_suggestions_default.dart';
+import 'package:sql_studio/src/notifiers/sql_suggestions_notifiers/sql_advanced_suggestions_notifier.dart';
 
 import 'package:sql_studio/src/shared/widgets/sql_workspace/sql_editor/sql_suggestions_bars/sql_suggestions_bar_widget.dart';
 
@@ -14,18 +15,20 @@ class SqlAdvancedSuggestionsBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final suggestions = context.watch<SqlAdvancedSuggestionsNotifier>().advancedSuggestions;
+
     return SqlSuggestionsBarBaseWidget(
       onTap: (index) {
-        final sqlAdvancedSuggestion = sqlAdvancedSuggestionsDefault[index];
+        final sqlAdvancedSuggestion = suggestions[index];
 
         onInsertCommand(
           sqlAdvancedSuggestion.code,
           selectText: sqlAdvancedSuggestion.selectText,
         );
       },
-      itemCount: sqlAdvancedSuggestionsDefault.length,
+      itemCount: suggestions.length,
       itemPadding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 10.0),
-      itemBuilder: (index) => sqlAdvancedSuggestionsDefault[index].label,
+      itemBuilder: (index) => suggestions[index].label,
     );
   }
 }
