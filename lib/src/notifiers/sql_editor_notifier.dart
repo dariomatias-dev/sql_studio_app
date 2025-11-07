a07ce3ad5cd15e9631d40e56b4c11a2efe35b8a8
dariomatias-dev <matiasdario75@gmail.com>
2025-11-07 14:42:39 -0300

    feat(lib/src/notifiers): created notifier for the code editor element
    - Developed a notifier for the code editor element to manage the component’s reactivity.

---
+++ lib/src/notifiers/sql_editor_notifier.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_code_editor/flutter_code_editor.dart';
+import 'package:highlight/languages/sql.dart';
+
+class SqlEditorNotifier extends ChangeNotifier {
+  final controller = CodeController(language: sql);
+  final focusNode = FocusNode();
+
+  SqlEditorNotifier() {
+    controller.addListener(_onTextChanged);
+    focusNode.addListener(_onFocusChanged);
+  }
+
+  String _lastWord = '';
+  String get lastWord => _lastWord;
+
+  bool _hasFocus = false;
+  bool get hasFocus => _hasFocus;
+
+  void _onTextChanged() {
+    final text = controller.text;
+    final lastWord = text.split(RegExp(r'\s+')).last.trim();
+
+    if (_lastWord != lastWord) {
+      _lastWord = lastWord;
+
+      notifyListeners();
+    }
+  }
+
+  void _onFocusChanged() {
+    if (_hasFocus != focusNode.hasFocus) {
+      _hasFocus = focusNode.hasFocus;
+
+      notifyListeners();
+    }
+  }
+
+  void clear() {
+    controller.text = '';
+    _lastWord = '';
+
+    notifyListeners();
+  }
+
+  void insertCommand(String command, {String? selectText}) {
+    final text = controller.text;
+    final selection = controller.selection;
+
+    if (text.isEmpty) {
+      controller.text = command;
+      controller.selection = TextSelection.collapsed(offset: command.length);
+
+      notifyListeners();
+
+      return;
+    }
+
+    final before = text.substring(0, selection.start);
+    final after = text.substring(selection.end);
+    final regex = RegExp(r'(\b\w+)$');
+    final match = regex.firstMatch(before);
+    final start = match != null ? match.start : selection.start;
+    final newText = before.replaceRange(start, before.length, command) + after;
+
+    controller.text = newText;
+    controller.selection = TextSelection.collapsed(
+      offset: start + command.length,
+    );
+
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_onTextChanged);
+    focusNode.removeListener(_onFocusChanged);
+    focusNode.dispose();
+
+    super.dispose();
+  }
+}
