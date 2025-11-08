05513948dcf4614a68b9d243fa04415a93ce77b1
dariomatias-dev <matiasdario75@gmail.com>
2025-11-08 19:39:37 -0300

    feat(lib/src/screens/sql_advanced_suggestion_settings/sql_advanced_suggestion_settings_screen.dart): implemented `SuggestionsSettingsLayoutWidget`
    - Simplified and standardized the screen with the implementation of `SuggestionsSettingsLayoutWidget`.

--- lib/src/screens/sql_advanced_suggestion_settings/sql_advanced_suggestion_settings_screen.dart
+++ lib/src/screens/sql_advanced_suggestion_settings/sql_advanced_suggestion_settings_screen.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:sql_studio/src/core/extensions/list_extension.dart';
-
 import 'package:sql_studio/src/notifiers/sql_suggestions_notifiers/sql_advanced_suggestions_notifier.dart';
 
 import 'package:sql_studio/src/screens/sql_advanced_suggestion_settings/sql_advanced_suggestion_settings_controller.dart';
 import 'package:sql_studio/src/screens/sql_advanced_suggestion_settings/widgets/sql_advanced_suggestion_card/sql_advanced_suggestion_card_widget.dart';
 
 import 'package:sql_studio/src/shared/models/sql_advanced_suggestion_model.dart';
-import 'package:sql_studio/src/shared/widgets/buttons/button_widget.dart';
-import 'package:sql_studio/src/shared/widgets/buttons/loading_button_widget.dart';
-import 'package:sql_studio/src/shared/widgets/scaffold_widget.dart';
+import 'package:sql_studio/src/shared/widgets/suggestions_settings_layout_widget.dart';
 
 class SqlAdvancedSuggestionSettingsScreen extends StatefulWidget {
   const SqlAdvancedSuggestionSettingsScreen({super.key});
@@ -36,89 +32,26 @@ class _SqlAdvancedSuggestionSettingsScreenState
   Widget build(BuildContext context) {
     final watchNotifier = context.watch<SqlAdvancedSuggestionsNotifier>();
 
-    return ScaffoldWidget(
-      appBar: AppBar(
-        title: const Text('Advanced Suggestions'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: _controller.showResetDialog,
-            icon: const Icon(Icons.refresh, color: Colors.black87),
-          ),
-        ],
-      ),
-      body: watchNotifier.isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Stack(
-              children: <Widget>[
-                ValueListenableBuilder<List<SqlAdvancedSuggestionModel>>(
-                  valueListenable: _controller.suggestionsOrderNotifier,
-                  builder: (context, suggestions, _) {
-                    return Theme(
-                      data: Theme.of(
-                        context,
-                      ).copyWith(canvasColor: Colors.white),
-                      child: ReorderableListView(
-                        padding: const EdgeInsets.only(
-                          top: 8.0,
-                          right: 12.0,
-                          bottom: 80.0,
-                          left: 12.0,
-                        ),
-                        onReorder: (oldIndex, newIndex) {
-                          _controller.reorderSuggestions(
-                            suggestions,
-                            oldIndex,
-                            newIndex,
-                          );
-                        },
-                        children: suggestions.builder((suggestion, index) {
-                          return SqlAdvancedSuggestionCardWidget(
-                            key: ValueKey(suggestion.id),
-                            suggestion: suggestion,
-                          );
-                        }),
-                      ),
-                    );
-                  },
-                ),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: Container(
-                    color: Colors.transparent,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                      vertical: 12.0,
-                    ),
-                    child: Row(
-                      spacing: 12.0,
-                      children: <Widget>[
-                        Expanded(
-                          child: SizedBox(
-                            height: 48.0,
-                            child: LoadingButtonWidget(
-                              text: 'Save',
-                              onPressed: () => _controller.saveOrder(
-                              ),
-                              style: ButtonStyleType.black,
-                            ),
-                          ),
-                        ),
-                        FloatingActionButton(
-                          onPressed: _controller.showCreateDialog,
-                          backgroundColor: Colors.grey.shade100,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(100.0),
-                          ),
-                          child: const Icon(Icons.add, color: Colors.black),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+    return SuggestionsSettingsLayoutWidget<SqlAdvancedSuggestionModel>(
+      title: 'Advanced Suggestions',
+      isLoading: watchNotifier.isLoading,
+      items: _controller.suggestionsOrderNotifier.value,
+      onReorder: (oldIndex, newIndex) {
+        _controller.reorderSuggestions(
+          _controller.suggestionsOrderNotifier.value,
+          oldIndex,
+          newIndex,
+        );
+      },
+      itemBuilder: (suggestion, index) {
+        return SqlAdvancedSuggestionCardWidget(
+          key: ValueKey(suggestion.id),
+          suggestion: suggestion,
+        );
+      },
+      onReset: _controller.showResetDialog,
+      onAdd: _controller.showCreateDialog,
+      onSave: _controller.saveOrder,
     );
   }
 }
