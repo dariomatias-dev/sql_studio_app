e76268cde150d0cf39db71d8ae88cdbbc9c6a0a6
dariomatias-dev <matiasdario75@gmail.com>
2025-11-08 19:40:10 -0300

    feat(lib/src/screens/sql_basic_suggestion_settings/sql_basic_suggestion_settings_screen.dart): implemented `SuggestionsSettingsLayoutWidget`
    - Simplified and standardized the screen with the implementation of `SuggestionsSettingsLayoutWidget`.

--- lib/src/screens/sql_basic_suggestion_settings/sql_basic_suggestion_settings_screen.dart
+++ lib/src/screens/sql_basic_suggestion_settings/sql_basic_suggestion_settings_screen.dart
@@ -6,7 +6,7 @@ import 'package:sql_studio/src/notifiers/sql_suggestions_notifiers/sql_basic_sug
 import 'package:sql_studio/src/screens/sql_basic_suggestion_settings/sql_basic_suggestion_settings_controller.dart';
 import 'package:sql_studio/src/screens/sql_basic_suggestion_settings/widgets/sql_basic_suggestion_card_widget.dart';
 
-import 'package:sql_studio/src/shared/widgets/scaffold_widget.dart';
+import 'package:sql_studio/src/shared/widgets/suggestions_settings_layout_widget.dart';
 
 class SqlBasicSuggestionsSettingsScreen extends StatefulWidget {
   const SqlBasicSuggestionsSettingsScreen({super.key});
@@ -23,55 +23,29 @@ class _SqlBasicSuggestionsSettingsScreenState
   @override
   Widget build(BuildContext context) {
     final notifier = context.watch<SqlBasicSuggestionsNotifier>();
-    final commands = controller.getSuggestions(notifier);
 
-    return ScaffoldWidget(
-      appBar: AppBar(
-        title: const Text('SQL Command Settings'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: controller.showResetConfirmationDialog,
-            icon: const Icon(Icons.refresh, color: Colors.black87),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: controller.showCreateCommandDialog,
-        backgroundColor: Colors.grey.shade100,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(100.0),
-        ),
-        child: const Icon(Icons.add, color: Colors.black),
-      ),
-      body: Theme(
-        data: Theme.of(context).copyWith(canvasColor: Colors.white),
-        child: ReorderableListView.builder(
-          padding: const EdgeInsets.only(
-            top: 8.0,
-            right: 12.0,
-            bottom: 80.0,
-            left: 12.0,
-          ),
-          itemCount: commands.length,
-          onReorder: (oldIndex, newIndex) {
-            controller.reorderSuggestions(
-              notifier,
-              commands,
-              oldIndex,
-              newIndex,
-            );
-          },
-          itemBuilder: (context, index) {
-            final cmd = commands[index];
-
-            return SqlBasicSuggestionCardWidget(
-              key: ValueKey(cmd),
-              command: cmd,
-              onDelete: () => controller.showRemoveCommandDialog(command: cmd),
-            );
-          },
-        ),
-      ),
+    return SuggestionsSettingsLayoutWidget<String>(
+      title: 'SQL Command Settings',
+      isLoading: false,
+      items: controller.getSuggestions(notifier),
+      onReorder: (oldIndex, newIndex) {
+        controller.reorderSuggestions(
+          notifier,
+          controller.getSuggestions(notifier),
+          oldIndex,
+          newIndex,
+        );
+      },
+      itemBuilder: (cmd, index) {
+        return SqlBasicSuggestionCardWidget(
+          key: ValueKey(cmd),
+          command: cmd,
+          onDelete: () => controller.showRemoveCommandDialog(command: cmd),
+        );
+      },
+      onReset: controller.showResetConfirmationDialog,
+      onAdd: controller.showCreateCommandDialog,
+      onSave: () async {},
     );
   }
 }
