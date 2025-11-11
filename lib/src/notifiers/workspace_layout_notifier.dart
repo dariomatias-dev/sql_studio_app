2e3a552fde8c8bbbb43fb7c6f407fd2f33336ca3
dariomatias-dev <matiasdario75@gmail.com>
2025-11-11 06:03:44 -0300

    feat(lib/src/notifiers/workspace_layout_notifier.dart): added result handling
    - Implemented result handling for the `setLayout` method.

--- lib/src/notifiers/workspace_layout_notifier.dart
+++ lib/src/notifiers/workspace_layout_notifier.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
 
+import 'package:sql_studio/src/core/result.dart';
 import 'package:sql_studio/src/core/constants/shared_preferences_keys.dart';
 import 'package:sql_studio/src/core/types/workspace_layout_type.dart';
 
 import 'package:sql_studio/src/services/shared_preferences_service.dart';
 
 class WorkspaceLayoutNotifier extends ChangeNotifier {
+  final logger = Logger();
+
   WorkspaceLayoutType _selectedLayout = WorkspaceLayoutType.split;
 
   WorkspaceLayoutType get selectedLayout => _selectedLayout;
@@ -24,14 +28,28 @@ class WorkspaceLayoutNotifier extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> setLayout(WorkspaceLayoutType layout) async {
-    _selectedLayout = layout;
+  Future<Result<void>> setLayout(WorkspaceLayoutType layout) async {
+    try {
+      _selectedLayout = layout;
 
-    await SharedPreferencesService.setString(
-      SharedPreferencesKeys.workspaceLayoutKey,
-      layout == WorkspaceLayoutType.split ? 'split' : 'tabs',
-    );
+      await SharedPreferencesService.setString(
+        SharedPreferencesKeys.workspaceLayoutKey,
+        layout == WorkspaceLayoutType.split ? 'split' : 'tabs',
+      );
 
-    notifyListeners();
+      notifyListeners();
+
+      return const SuccessResult(null);
+    } catch (err, stackTrace) {
+      logger.e(
+        'Error saving workspace layout',
+        error: err,
+        stackTrace: stackTrace,
+      );
+
+      return FailureResult(
+        AppFailure('Failed to save workspace layout. Please try again.'),
+      );
+    }
   }
 }
