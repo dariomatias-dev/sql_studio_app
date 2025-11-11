ca757c030f81c461120fa6d2f7e013991fd54df9
dariomatias-dev <matiasdario75@gmail.com>
2025-11-11 06:06:30 -0300

    feat(lib/src/screens/workspace_layout/workspace_layout_screen.dart): added layout save handling
    - Created `_onHandleLayoutChange` method to handle layout saving.

--- lib/src/screens/workspace_layout/workspace_layout_screen.dart
+++ lib/src/screens/workspace_layout/workspace_layout_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:sql_studio/src/core/result.dart';
 import 'package:sql_studio/src/core/types/workspace_layout_type.dart';
+
 import 'package:sql_studio/src/notifiers/workspace_layout_notifier.dart';
+
 import 'package:sql_studio/src/screens/workspace_layout/widgets/workspace_layout_option_card_widget.dart';
+
+import 'package:sql_studio/src/shared/utils/handle_error.dart';
 import 'package:sql_studio/src/shared/widgets/scaffold_widget.dart';
 import 'package:sql_studio/src/shared/widgets/sql_workspace/sql_workspace_widget.dart';
+import 'package:sql_studio/src/shared/utils/snack_bar_utils.dart';
 
-class WorkspaceLayoutScreen extends StatelessWidget {
+class WorkspaceLayoutScreen extends StatefulWidget {
   const WorkspaceLayoutScreen({super.key});
+
+  @override
+  State<WorkspaceLayoutScreen> createState() => _WorkspaceLayoutScreenState();
+}
+
+class _WorkspaceLayoutScreenState extends State<WorkspaceLayoutScreen> {
+  Future<void> _onHandleLayoutChange(WorkspaceLayoutType layout) async {
+    final result = await context.read<WorkspaceLayoutNotifier>().setLayout(
+      layout,
+    );
+
+    if (!mounted) return;
+
+    if (result is FailureResult) {
+      await handleError(context, result);
+    } else {
+      SnackBarUtils.show(context, 'Layout saved');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +42,7 @@ class WorkspaceLayoutScreen extends StatelessWidget {
         final selectedLayout = notifier.selectedLayout;
 
         return ScaffoldWidget(
-          appBar: AppBar(title: Text('Workspace Layout')),
+          appBar: AppBar(title: const Text('Workspace Layout')),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
@@ -27,7 +52,7 @@ class WorkspaceLayoutScreen extends StatelessWidget {
                   title: 'Split Layout',
                   subtitle: 'Editor above and console below.',
                   selected: selectedLayout == WorkspaceLayoutType.split,
-                  onTap: () => notifier.setLayout(WorkspaceLayoutType.split),
+                  onTap: () => _onHandleLayoutChange(WorkspaceLayoutType.split),
                 ),
                 const SizedBox(height: 8.0),
                 WorkspaceLayoutOptionCardWidget(
@@ -35,12 +60,12 @@ class WorkspaceLayoutScreen extends StatelessWidget {
                   title: 'Tabs Layout',
                   subtitle: 'Editor and console in tabs.',
                   selected: selectedLayout == WorkspaceLayoutType.tabs,
-                  onTap: () => notifier.setLayout(WorkspaceLayoutType.tabs),
+                  onTap: () => _onHandleLayoutChange(WorkspaceLayoutType.tabs),
                 ),
                 const SizedBox(height: 28.0),
-                Align(
-                  alignment: AlignmentGeometry.centerLeft,
-                  child: const Text(
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
                     'Preview:',
                     style: TextStyle(
                       color: Colors.black87,
